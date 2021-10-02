94e6a4f8b4485bf74dc34447ee1030d27b793c6b
Roman DILMI <roman.dilmi@orange.fr>
2021-10-02 23:54:58 +0200

    done, need testing

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ app.whenReady().then(() => {
   globalShortcut.register('R', () => {
     console.log('Electron loves global shortcuts!');
 
+    robot.typeString('r');
+
+    //Open inventary
+    robot.typeString('b');
+
     switchGears();
 
-    // Type "Hello World".
-    // robot.typeString("Hello World");
+    robot.typeString('b');
+
   })
 })
 
 const switchGears = () => {
     getPos();
+
+    // Gloves
+    switchGear({x: 630, y: 880}, {x: 633, y: 815});
 }
 
 const getPos = () => {
     const mouse = robot.getMousePos();
     console.log('mouse', mouse);
+}
+
+const switchGear = (posOrigin, posDestination) => {
+    robot.moveMouse(posOrigin.x, posOrigin.y);
+    robot.mouseToggle("down");
+    robot.dragMouse(posDestination.x, posDestination.y);
+    robot.mouseToggle("up");
+    robot.moveMouse(posOrigin.x, posOrigin.y);
+    robot.mouseClick();
 }
